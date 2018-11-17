9a61d893c3b7973f249381ffce912765e7f40a25
cs16048 <arrowsuke@yahoo.co.jp>
2018-11-18 01:15:43 +0900

    初期プログラム

---
+++ Assets/BulletController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    public GameObject explosionPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0.2f, 0);
+
+        if (transform.position.y > 5)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(coll.gameObject);
+        Destroy(gameObject);
+    }
+}
