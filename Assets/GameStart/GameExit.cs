f6fcc390dff59b0f5d96e0f374f0dd70cdefeb13
cs16048 <arrowsuke@yahoo.co.jp>
2018-11-18 23:10:42 +0900

    スタート画面時のExitボタンの作成と背景

---
+++ Assets/GameStart/GameExit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameExit : MonoBehaviour {
+
+	public void toGameExit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+
+        Application.Quit();
+
+    }
+}
