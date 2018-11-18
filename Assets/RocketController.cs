6670eb070bbb06451d6671d4ba8e5877512fbffa
cs16048 <arrowsuke@yahoo.co.jp>
2018-11-18 19:39:31 +0900

    音声の追加と操作範囲の設定

--- Assets/RocketController.cs
+++ Assets/RocketController.cs
@@ -2,28 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RocketController : MonoBehaviour {
+public class RocketController : MonoBehaviour
+{
 
     public GameObject bulletPrefab;
+    private AudioSource laser2;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey (KeyCode.LeftArrow))
+    // Use this for initialization
+    void Start()
+    {
+        laser2 = GetComponent<AudioSource>();
+        transform.position = new Vector3(0, -4, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = this.gameObject.transform.position;
+
+        if (pos.x > -2.22 && pos.x < 2.22)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                this.gameObject.transform.position = new Vector3(pos.x - 0.1f, pos.y, pos.z);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                this.gameObject.transform.position = new Vector3(pos.x + 0.1f, pos.y, pos.z);
+            }
+        }
+
+        else if (pos.x >= 2.22)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                this.gameObject.transform.position = new Vector3(pos.x - 0.1f, pos.y, pos.z);
+            }
+        }
+
+        else if (pos.x <= -2.22)
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                this.gameObject.transform.position = new Vector3(pos.x + 0.1f, pos.y, pos.z);
+            }
+        }
+
+        /*if (Input.GetKey (KeyCode.LeftArrow))
         {
             transform.Translate(-0.1f, 0, 0);
         }
         if (Input.GetKey (KeyCode.RightArrow))
         {
             transform.Translate(0.1f, 0, 0);
-        }
-        if (Input.GetKeyDown (KeyCode.Space))
+        }*/
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            laser2.PlayOneShot(laser2.clip);
         }
-	}
+    }
 }
+
