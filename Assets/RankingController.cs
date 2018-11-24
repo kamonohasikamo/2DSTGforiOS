1f31438a532722a8fc01bcfdb9338323dee27ed4
cs16048 <arrowsuke@yahoo.co.jp>
2018-11-24 16:32:11 +0900

    ランキング機能の実装

---
+++ Assets/RankingController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankingController : MonoBehaviour {
+
+    [SerializeField] Text ranking;
+    private int[] sort = new int[6];
+    private int tmp = 0;
+
+    // Use this for initialization
+    void Start () {
+        ranking.GetComponent<Text>().text = "";
+        saveRanking();
+        printRanking();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void printRanking()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            sort[i] = PlayerPrefs.GetInt(""+i,0);
+            Debug.Log(sort[i]);
+        }
+        for (int start = 1; start < sort.Length; start++)
+        {
+            for (int end = sort.Length - 1; end >= start; end--)
+            {
+                if (sort[end - 1] <= sort[end])
+                {
+                    tmp = sort[end - 1];
+                    sort[end - 1] = sort[end];
+                    sort[end] = tmp;
+                }
+            }
+        }
+        bool f = true;
+        for (int i = 0; i < 5; i++)
+        {
+            if (f && UIController.score == sort[i])
+            {
+                ranking.GetComponent<Text>().text += "<color=red>" + (i + 1) + "位： Score  " + sort[i].ToString("D4") + "</color>\n";
+                f = false;
+
+            }
+            else
+            {
+                ranking.GetComponent<Text>().text += i + 1 + "位： Score  " + sort[i].ToString("D4") + "\n";
+                PlayerPrefs.SetInt("" + i, sort[i]);
+
+            }
+        }
+        PlayerPrefs.Save();
+    }
+
+    void saveRanking()
+    {
+        PlayerPrefs.SetInt("5", UIController.score);
+        PlayerPrefs.Save();
+    }
+}
---
+++ Assets/resultScoreController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class resultScoreController : MonoBehaviour
+{
+
+    GameObject resultScoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.resultScoreText = GameObject.Find("resultScoreText");
+        resultScoreText.GetComponent<Text>().text = "Score:" + UIController.score.ToString("D4");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
